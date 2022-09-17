d9376c35be069cb999c3a94e752915c2e160d82c
KehlanMetor <82890272+KehlanMetor@users.noreply.github.com>
2022-09-18 00:20:03 +0200

    Update preloader.js

--- js/preloader.js
+++ js/preloader.js
@@ -60,7 +60,7 @@ var strings = [
   "Loading content",
   "Page rendered",
   "Starting display manager",
-  "WELCOME TO LAKSHYA CTF INC 2020",
+  "WELCOME TO Pingu CTF INC 2022",
   "Initializing..."
 ];
 
